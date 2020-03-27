5e375091616b6981e0f09e40858e417934a18c38
olov <olov@2abf716187cd>
2020-03-27 15:51:29 +0000

    javadoc updates

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /**
- * Module logger provides interfaces and access needed to use logging in a Cora based system. The
- * logger implementation in use can be accessed through the {@link LoggerProvider} class and its
+ * The logger module provides interfaces and access needed to use logging in a Cora based system.
+ * The logger implementation in use can be accessed through the {@link LoggerProvider} class and its
  * getLoggerForClass method.
  * <p>
  * Logging implementations are provided through other modules and MUST implement the
